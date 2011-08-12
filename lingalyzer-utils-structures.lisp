43e8eaa733dc547690d631420b129a2f9ace77ae
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2011-08-12 10:29:02 +0200

    Simplified the structure used to store documents and related information

--- lingalyzer-utils-structures.lisp
+++ lingalyzer-utils-structures.lisp
@@ -1,10 +1,10 @@
 (in-package :org.kjerkreit.lingalyzer.utils)
 
-(defstruct author
+(defstruct agent
   (name   "John Doe"  :type string :read-only t)
   (ngrams nil         :type list   :read-only t)
-  (b-day  "000-00-00" :type string :read-only t)
-  (d-day  "000-00-00" :type string :read-only t)
+  (bday   "000-00-00" :type string)
+  (dday   "000-00-00" :type string)
   (docs   nil         :type list))
 
 (defstruct term
@@ -12,32 +12,16 @@
   (ngrams nil :type list :read-only t)
   (count  0   :type integer))
 
-(defstruct copyist
-  (name     "John Doe"  :type string :read-only t)
-  (ngrams   nil         :type list   :read-only t)
-  (b-day    "000-00-00" :type string :read-only t)
-  (d-day    "000-00-00" :type string :read-only t)
-  (docs     nil         :type list)
-  (employer ""          :type string :read-only t))
-
-(defstruct doc
+(defstruct doc-container
   (name          "A tale" :type string :read-only t)
-  (name-hash     "-"      :type string :read-only t)
-  (ngrams        nil      :type list   :read-only t)
-  (author        nil      :type author)
+  (author        nil      :type )
   (genre         "-"      :type string)
+  (versions      nil      :type list))
+
+(defstruct doc-version
   (version       0        :type integer)
-  (copied-by     nil      :type copyist)
+  (copied-by     nil      :type )
   (word-count    0        :type integer)
   (org-file      "-"      :type string)
   (org-file-hash "-"      :type string)
-  (terms         nil      :type list))
-
-(defstruct translator
-  (name     "John Doe"  :type string :read-only t)
-  (ngrams   nil         :type list   :read-only t)
-  (b-day    "000-00-00" :type string :read-only t)
-  (d-day    "000-00-00" :type string :read-only t)
-  (docs     nil         :type list)
-  (employer "John Doe"  :type string :read-only t))
-  
+  (terms         nil      :type list))  
