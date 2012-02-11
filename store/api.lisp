4cb8fba255db3c5a81b7d06b90899c555e8de72e
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2012-02-11 15:10:40 +0100

    OO based design is out with regards to the store.

--- store/api.lisp
+++ store/api.lisp
@@ -1,56 +1,63 @@
 (in-package :org.kjerkreit.lingalyzer.store)
 
-(declaim (optimize (debug 3) (safety 3) (speed 0) (space 0)))
-;(declaim (optimize (debug 0) (safety 0) (speed 3) (space 0)))
+#-la-full-speed (declaim (optimize (debug 3) (safety 3) (speed 0) (space 0)))
+#+la-full-speed (declaim (optimize (debug 0) (safety 0) (speed 3) (space 0)))
 
 ;;;; INIT
 
 (defun new-store (name)
   "Create a new store."
 
+  (setf *default-store* (make-instance '
   (if (probe-file name)
       (error "Store appears to already exist.")
       (ensure-directories-exist name :mode 555)))
 
 ;;;; Store
 
-(defmethod close-store ()
-  "Closes the store.")
+(defun close-store ()
+  "Closes the store."
 
-(defmethod open-store (name)
+  (__close-store *default-store*))
+
+(defun open-store (name)
   "Open a store. Close the currently open store (if one exists).")
 
 
 ;;;; Store: content - general
 
-(defmethod add-ent (entity)
-  "Add an entity to the store.")
+(defun add-ent (entity)
+  "Add an entity to the store."
 
-(defmethod remove-ent (type key)
+  (__add-enttity *default-store* entity))
+
+(defun remove-ent (type key)
   "Remove an entity from the store.")
 
 
 ;;;; DB: content - general
 
-(defmethod exists-p (type key)
-  "Synonym for (get-ent ...)")
+(defun exists-p (type key)
+  "Synonym for (get-ent ...)"
 
-(defmethod get-all-ent ()
+  (get-ent type key))
+
+(defun get-all-ent ()
   "Get all entities in the store.")
 
-(defmethod get-ent (type key)
+(defun get-ent (type key)
   "Get matching entity of specified type from the store.")
 
-(defmethod get-ent-by (type attribyte value)
+(defun get-ent-by (type attribyte value)
   "Get all entities mathing value of specified attribute.")
 
-(defmethod update-ent (entity)
+(defun update-ent (entity)
   "Update an entity in the store.")
 
 ;;;; Index
 
-(defmethod find-entities (query &optional (type nil) (threshold 0.65))
+(defun find-entities (query &optional (type nil) (threshold 0.65))
   "Search the index for a matching key.")
 
-(defmethod indexed-p (type key)
+(defun indexed-p (type key)
   "Is the key in the index?")
