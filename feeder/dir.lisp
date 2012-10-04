9b82fe8fb7c797850810eb93910cdb0d215a832b
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2012-10-04 19:04:08 +0200

    Improved feeder:dir() docstring

--- feeder/dir.lisp
+++ feeder/dir.lisp
@@ -1,7 +1,8 @@
 (in-package :org.kjerkreit.lingalyzer.feeder)
 
 (defun read-dir (path)
-  "Reads all (text) files in a directory."
+  "Reads all (text) files in a directory. Returns a list of the result of applying read-file() to
+all files in the directory (and sub-directories)."
 
   (let ((files))
     (cl-fad:walk-directory path #'(lambda (f)
